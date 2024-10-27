ab76ff6f9829256a6db6bf9e3dfc2f84fa14b9e3
Kate465 <kategokeborate@gmail.com>
2024-10-27 22:33:30 +0000

    added form handling and typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-
+function generatePoem(event) {
+  event.preventDefault();
+  new Typewriter("#poem", {
+    strings: "Je partirai. Vois-tu, je sais que tu m’attend",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
